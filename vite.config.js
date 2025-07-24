d1da7a151073bbbaca9965c747eb594ae50dbec4
Luis Barrichello <luisgbarrichelo@gmail.com>
2025-07-24 17:01:03 -0300

    chore: setup tailwind and theme provider

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/leetcode-api': {
-        target: 'https://leetcode.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/leetcode-api/, '')
-      }
-    }
-  }
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            '/leetcode-api': {
+                target: 'https://leetcode.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/leetcode-api/, ''),
+            },
+        },
+    },
+});
